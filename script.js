9f4cdbd769cd0a021e2d23293eb813ff36d74c6c
José Rodolpho <j.rodolpho29@hotmail.com>
2022-02-11 23:43:41 -0300

    Criação da conexão da API 'messages' com o site

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ function keepConnected(){
 login()
 setInterval(keepConnected, 5000)
 
+function showValue(){
+    let message = document.querySelector(".write-message")
+    let value = message.value
+}
+
+function getServerMessage(){
+    const serverMessage = axios.get(`${baseUrl}/messages`)
+    serverMessage.then(showMessage)
+}
+
+function showMessage(serverMessage){
+    let serverMessages = serverMessage.data
+    for(let i = 0; i < serverMessages.length;i++){
+        if(serverMessages[i].type === "status"){
+            document.querySelector(".chat").innerHTML += `<p>${serverMessages[i].from} ${serverMessages[i].text}</p>`
+        }
+        else if(serverMessages[i].type === "message"){
+            document.querySelector(".chat").innerHTML += `<p>${serverMessages[i].from} para: ${serverMessages[i].to}: ${serverMessages[i].text}</p>`
+        }
+    }
+}
+
+setInterval(getServerMessage, 3000)
